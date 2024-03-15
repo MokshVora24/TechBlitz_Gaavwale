e7e0c624439e2a76208792450a6f38114cd7a4cb
parthpsg <66634291+parthgala1@users.noreply.github.com>
2024-03-15 13:30:01 +0530

    dekhle na

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import { useAuthContext } from '../src/context/AuthContext'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import HeroScrollDemo from "./pages/TopVolunteers";
+import Navbar from "./components/Navbar";
+import OrderMedicinePage from "./pages/OrderMedicine";
+import MapboxComponent from "./pages/Map";
+import OrderGroceriesPage from "./pages/OrderGroceries";
+// import './App.css'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Home from '../src/pages/Home.jsx'
 
 function App() {
-  const authUser = useAuthContext()
-
   return (
+
     <>
-      <div className='p-4 h-screen flex items-center justify-center'>
+
+      {/* <MapboxComponent /> */}
+
+      <BrowserRouter>
 
         <Routes>
-
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<>
+            <Navbar />
+            <Home />
+            <HeroScrollDemo href="#our-volunteers" />
+            <OrderMedicinePage href="#order-medicines" />
+            <OrderGroceriesPage href="#order-groceries" />
+          </>} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-        {/* <Toaster /> */}
-      </div>
+      </BrowserRouter>
+      {/* <Toaster /> */}
     </>
   )
 }
